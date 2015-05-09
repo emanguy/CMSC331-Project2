caf04ab70ae791405d6eaadc6841540746d7f18f
Evan Rittenhouse <emanguy1@gmail.com>
2015-05-08 22:37:29 -0400

    Initial creation of welcome page

---
+++ advisors/Welcome.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+    <title>Advisor Control Panel </title>
+
+    <!-- Import common styles -->
+    <link rel="stylesheet" href="PageStyle.css" type="text/css">
+
+    <!-- Page-specific styles -->
+    <style type="text/css">
+        form
+        {
+            display: inline;
+            margin: 4px;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Paw print logo -->
+    <img height="10%" src="http://assets1-my.umbc.edu/images/avatars/myumbc/original.png?1425393485"></img><br>
+
+    <!-- Page title. PHP retrieves the advisor's name. -->
+    <center>
+        <font size="18" color="red" face="Tw Cen MT">Welcome, 
+        <?php
+                    // Get advisor ID from POST
+                    $advisorID = $_POST["advID"];
+
+                    // Connect to DB
+                    // $DB = new Common(false);
+                    $query = "SELECT `Name` FROM `Advisor` WHERE `ID` = ".$advisorID;
+
+                    // Get the advisor's name
+                    $advisorName = MYSQL_FETCH_ARRAY($DB->executeQuery($query, $_SERVER["SCRIPT_NAME"]));
+                    print($advisorName["Name"]);
+    ?>!</font>
+    </center>
+
+    <!-- Box on page -->
+    <div class="centerBox">
+
+        <!-- "Control Panel" title -->
+        <center>
+            <h2>YOUR CONTROL PANEL</h2>
+        </center>
+
+        <!-- Navigation buttons to other pages. Every button sends the POST variable "advID" -->
+        <center>
+            <form action="EventSelect.php" method="POST">
+                <input type="hidden" name="advID" value=<?php print("'".$_POST["advID"]."'"); ?>>
+                <input type="submit" value="List Templates">
+            </form>
+            <form action="ApptDayPick.php" method="POST">
+                <input type="hidden" name="advID" value=<?php print("'".$_POST["advID"]."'"); ?>>
+                <input type="submit" value="View Appointments">
+            </form>
+            <form action="TemplateWeek.php" method="POST">
+                <input type="hidden" name="advID" value=<?php print("'".$_POST["advID"]."'"); ?>>
+                <input type="submit" value="Set Default Templates">
+            </form>
+        </center>
+    </div>
+</body>
+</html>
